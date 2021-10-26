d9eb5472056d95cefb7c01d256fc9e0ce7c82abb
Wahyu Agus Arifin - IP <itpohgero@gmail.com>
2021-10-26 23:05:24 +0700

    Perubahan

--- source/screens/home/HomeScreen.js
+++ source/screens/home/HomeScreen.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {StyleSheet, View, Text} from 'react-native';
 import {Slider, Header, FlashInfo} from './components';
 
 const HomeScreen = ({navigation}) => {
   return (
     <View style={{flex: 1}}>
+      <Text>lorem</Text>
       <Header navigation={navigation} />
       <Slider />
       <FlashInfo />
